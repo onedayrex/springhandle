ca9ea7ea0159229008cf70bc7c8d8747531db3dd
onedayrex <1115757866@qq.com>
2018-05-10 15:06:06 +0800

    get httpServletRequest

--- src/main/java/com/onedayrex/git/springhandle/config/UserInfoHandleResolver.java
+++ src/main/java/com/onedayrex/git/springhandle/config/UserInfoHandleResolver.java
@@ -7,6 +7,8 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import javax.servlet.http.HttpServletRequest;
+
 public class UserInfoHandleResolver implements HandlerMethodArgumentResolver {
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -15,6 +17,8 @@ public class UserInfoHandleResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest req = (HttpServletRequest) webRequest.getNativeRequest();
+        String param = req.getParameter("user");
         User user = new User();
         user.setUserName("tom");
         user.setPassWord("123456");
