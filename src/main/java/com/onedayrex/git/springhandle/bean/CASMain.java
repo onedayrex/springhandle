72598b1f6fedfe9d17179dfec59dc7b720b51833
onedayrex <1115757866@qq.com>
2018-05-11 13:33:05 +0800

    CAS乐观锁

---
+++ src/main/java/com/onedayrex/git/springhandle/bean/CASMain.java
@@ -0,0 +1,19 @@
+package com.onedayrex.git.springhandle.bean;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class CASMain {
+
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        CASUpdate casUpdate = new CASUpdate(1);
+        for (int i = 0; i < 20; i++) {
+            executorService.execute(new CASRunnAble(casUpdate));
+        }
+        countDownLatch.await();
+    }
+}
