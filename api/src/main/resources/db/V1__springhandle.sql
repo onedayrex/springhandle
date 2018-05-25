57e22a1139d5abad5eba7982569579ed298d95d5
onedayrex <1115757866@qq.com>
2018-05-25 16:47:31 +0800

    flyway

--- api/src/main/resources/db/V1__springhandle.sql
+++ api/src/main/resources/db/V1__springhandle.sql
@@ -1,12 +1,10 @@
+-- flyway自己根据版本执行sql 脚本名称格式 V1__xxx.sql
 DROP TABLE IF EXISTS `user_info`;
 CREATE TABLE `user_info` (
 	`id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id 编号',
-  `
-  create_time` timestamp default null COMMENT '创建时间',
-  `update_time` timestamp default null COMMENT '更新时间',
+  `create_time` timestamp COMMENT '创建时间',
+  `update_time` timestamp COMMENT '更新时间',
 	`user_name` VARCHAR(30) DEFAULT NULL,
 	`pass_word` VARCHAR(30) DEFAULT NULL,
 	PRIMARY KEY (`id`)
 );
-
-insert into user_info(user_name,pass_word) values ('tom','123');
---
+++ api/src/main/resources/db/V2__springhandle.sql
@@ -0,0 +1,1 @@
+insert into user_info(user_name,pass_word) values ('tom','123');
